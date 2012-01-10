e8bef6342c38f9185ced4f66d7431b846b2a65e2
rf <rf0444@gmail.com>
2012-01-10 19:50:21 +0900

    view 側ざっくり作成

--- src/app/controllers.scala
+++ src/app/controllers.scala
@@ -4,11 +4,8 @@ import play._
 import play.mvc._
 
 object Application extends Controller {
-    
     import views.Application._
     
-    def index = {
-        html.index("Your Scala application is ready!")
-    }
-    
+    def index = html.index("memo test")
 }
+
---
+++ src/public/javascripts/index.js
@@ -0,0 +1,5 @@
+
+$(function() {
+	$(".memo").focus();
+});
+
