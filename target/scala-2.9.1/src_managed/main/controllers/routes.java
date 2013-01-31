bc53c261a9035a7e9ea7f28575d905ff31ebffd5
shiganaiman <shiganaiman@gmail.com>
2013-01-31 19:10:03 +0900

    init project

---
+++ target/scala-2.9.1/src_managed/main/controllers/routes.java
@@ -0,0 +1,19 @@
+// @SOURCE:/home/ws_nakada/workspace/scutras/scutras/conf/routes
+// @HASH:f904f23acbfbb0ded1d9ae85b262187e14e562de
+// @DATE:Thu Jan 31 19:03:01 JST 2013
+
+package controllers;
+
+public class routes {
+public static final controllers.ReverseApplication Application = new controllers.ReverseApplication();
+public static final controllers.ReverseAssets Assets = new controllers.ReverseAssets();
+public static class javascript {
+public static final controllers.javascript.ReverseApplication Application = new controllers.javascript.ReverseApplication();
+public static final controllers.javascript.ReverseAssets Assets = new controllers.javascript.ReverseAssets();    
+}   
+public static class ref {
+public static final controllers.ref.ReverseApplication Application = new controllers.ref.ReverseApplication();
+public static final controllers.ref.ReverseAssets Assets = new controllers.ref.ReverseAssets();    
+} 
+}
+                            
