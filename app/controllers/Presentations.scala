8acd6904085927f054327c08452d09f6e566fa9c
shiganaiman <shiganaiman@gmail.com>
2013-02-08 21:00:07 +0900

    commit

---
+++ app/controllers/Presentations.scala
@@ -0,0 +1,17 @@
+package controllers
+
+import play.api.mvc.Action
+import play.api.mvc.Controller
+
+object Presentations extends Controller {
+  
+  def list = Action {
+    Ok(views.html.index("a presentation."))
+  }
+
+  def index(id:Long) = TODO
+  def create = TODO
+  def delete(id:Long) = TODO
+  
+
+}
