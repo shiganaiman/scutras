aef883e7e52446ce4ebc4dc1546828e4a7af8f11
shiganaiman <shiganaiman@gmail.com>
2013-02-13 13:19:44 +0900

    ちょっとだけよ

--- project/Build.scala
+++ project/Build.scala
@@ -9,11 +9,14 @@ object ApplicationBuild extends Build {
 
   val appDependencies = Seq(
     // Add your project dependencies here,
-    "com.artofsolving" % "jodconverter" % "2.2.1",
+   "se.radley" %% "play-plugins-salat" % "1.2",
+   "com.artofsolving" % "jodconverter" % "2.2.1",
     jdbc,
     anorm)
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-      requireJs += "main.js"
+      requireJs += "main.js",
+      routesImport += "se.radley.plugin.salat.Binders._",
+      templatesImport += "org.bson.types.ObjectId"
 	)
 }
