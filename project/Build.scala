f29b1196a572a7e951633cccdb022e7f84ff9d1f
shiganaiman <shiganaiman@gmail.com>
2013-02-22 16:46:04 +0900

    initial project

--- project/Build.scala
+++ project/Build.scala
@@ -6,17 +6,17 @@ object ApplicationBuild extends Build {
 
   val appName = "scutras"
   val appVersion = "1.0-SNAPSHOT"
-
   val appDependencies = Seq(
     // Add your project dependencies here,
+//   jdbc,
+//   anorm,
    "se.radley" %% "play-plugins-salat" % "1.2",
-   "com.artofsolving" % "jodconverter" % "2.2.1",
-    jdbc,
-    anorm)
+   "com.artofsolving" % "jodconverter" % "2.2.1")
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
-      requireJs += "main.js",
-      routesImport += "se.radley.plugin.salat.Binders._",
-      templatesImport += "org.bson.types.ObjectId"
+		  resolvers += "Sonatype OSS Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots",
+		  requireJs += "main.js",
+		  routesImport += "se.radley.plugin.salat.Binders._",
+		  templatesImport += "org.bson.types.ObjectId"
 	)
 }
