8db8578cf040541dfd73afbe0e3479e2c73f7b29
shiganaiman <shiganaiman@gmail.com>
2013-02-22 16:46:04 +0900

    initial project

--- app/persists/User.scala
+++ app/persists/User.scala
@@ -1,29 +1,25 @@
 package persists
 
 import play.api.Play.current
-import java.util.Date
-import com.novus.salat._
-import com.novus.salat.annotations._
 import com.novus.salat.dao._
 import com.mongodb.casbah.Imports._
 import se.radley.plugin.salat._
-import mongoContext._
-import com.sun.corba.se.spi.ior.ObjectId
 
 case class User(
-  id: ObjectId = new ObjectId,
-  username: String,
-  password: String,
-  address: Option[Address] = None,
-  added: Date = new Date(),
-  updated: Option[Date] = None,
-  deleted: Option[Date] = None,
-  @Key("company_id")company: Option[ObjectId] = None
-)
+                 id: ObjectId = new ObjectId,
+                 account: String,
+                 username: String,
+                 password: String,
+                 email: Option[String] = None) {
+  def equals(that: Any): Boolean = {
+    return super.equals(that);
+  }
+}
 
 object User extends ModelCompanion[User, ObjectId] {
   val dao = new SalatDAO[User, ObjectId](collection = mongoCollection("users")) {}
 
-  def findOneByUsername(username: String): Option[User] = dao.findOne(MongoDBObject("username" -> username))
-  def findByCountry(country: String) = dao.find(MongoDBObject("address.country" -> country))
+  def findOneByUsername(username: String): Option[User] = {
+    dao.findOne(MongoDBObject("username" -> username))
+  }
 }
